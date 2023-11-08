546f0e1edbc0178040d6d5749013001e024daea1
ishanyasingh <ishanya.singh2021@vitstudent.ac.in>
2023-11-09 01:00:32 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('https://dummyjson.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        // Set the fetched data in the state
+        setUserData(data.users);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Dummy Data</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Sno</th>
+            <th>Profile Pic</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+            {/* Add more table headers as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td><img src={user.image} alt='img'/></td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.gender}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+              <td>{user.domain}</td>
+              <td>{user.ip}</td>
+              <td>{user.university}</td>
+              {/* Add more table data cells as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
